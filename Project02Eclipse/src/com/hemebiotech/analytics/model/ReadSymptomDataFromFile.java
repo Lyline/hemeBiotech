7bb0dfa063ac73debb468d5e7a0a3c575050890d
Lyline <greg.quesne@gmail.com>
2021-03-25 16:11:26 +0100

    Fix suppress redondant variable

--- Project02Eclipse/src/com/hemebiotech/analytics/model/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/model/ReadSymptomDataFromFile.java
@@ -51,7 +51,7 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 	 */
 	public Symptom addSymptomType(String element) {
 
-		Symptom typedSymptom = switch (element) {
+		return switch (element) {
 			case "high blood pressure", "low blood pressure", "rapid heart rate", "arrhythmias"
 					-> new Symptom(element, SymptomType.Cardiac);
 			case "blindness", "blurred vision", "constricted pupils", "dialated pupils"
@@ -71,7 +71,6 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 
 			default-> new Symptom(element, null);
 		};
-		return typedSymptom;
 	}
 
 	/**
@@ -86,5 +85,85 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 			symptomMap.put(symptom.getName(),symptomMap.getOrDefault(symptom.getName(), 0) + 1);
 		}
 		return symptomMap;
+	}
+
+	public static void sortByCardiac(List<Symptom>symptomList){
+		Map<String,Long> map=symptomList.stream()
+				.filter(s->s.getType()==SymptomType.Cardiac)
+				.collect(Collectors.groupingBy(Symptom::getName,Collectors.counting()));
+
+		System.out.println("--- Cardiac Symptom ---");
+		map.forEach((s, aLong) -> System.out.println(s+" : "+aLong));
+		System.out.println("-----------------------\n");
+	}
+
+	public static void sortByVision(List<Symptom>symptomList){
+		Map<String,Long> map=symptomList.stream()
+				.filter(s->s.getType()==SymptomType.Vision)
+				.collect(Collectors.groupingBy(Symptom::getName,Collectors.counting()));
+
+		System.out.println("--- Vision Symptom ---");
+		map.forEach((s, aLong) -> System.out.println(s+" : "+aLong));
+		System.out.println("-----------------------\n");
+	}
+
+	public static void sortByRespiratory(List<Symptom>symptomList){
+		Map<String,Long> map=symptomList.stream()
+				.filter(s->s.getType()==SymptomType.Respiratory)
+				.collect(Collectors.groupingBy(Symptom::getName,Collectors.counting()));
+
+		System.out.println("--- Respiratory Symptom ---");
+		map.forEach((s, aLong) -> System.out.println(s+" : "+aLong));
+		System.out.println("-----------------------\n");
+	}
+
+	public static void sortByMuscular(List<Symptom>symptomList){
+		Map<String,Long> map=symptomList.stream()
+				.filter(s->s.getType()==SymptomType.Muscular)
+				.collect(Collectors.groupingBy(Symptom::getName,Collectors.counting()));
+
+		System.out.println("--- Muscular Symptom ---");
+		map.forEach((s, aLong) -> System.out.println(s+" : "+aLong));
+		System.out.println("-----------------------\n");
+	}
+
+	public static void sortByGastric(List<Symptom>symptomList){
+		Map<String,Long> map=symptomList.stream()
+				.filter(s->s.getType()==SymptomType.Gastric)
+				.collect(Collectors.groupingBy(Symptom::getName,Collectors.counting()));
+
+		System.out.println("--- Gastric Symptom ---");
+		map.forEach((s, aLong) -> System.out.println(s+" : "+aLong));
+		System.out.println("-----------------------\n");
+	}
+
+	public static void sortByPain(List<Symptom>symptomList){
+		Map<String,Long> map=symptomList.stream()
+				.filter(s->s.getType()==SymptomType.Pain)
+				.collect(Collectors.groupingBy(Symptom::getName,Collectors.counting()));
+
+		System.out.println("--- Pain Symptom ---");
+		map.forEach((s, aLong) -> System.out.println(s+" : "+aLong));
+		System.out.println("-----------------------\n");
+	}
+
+	public static void sortByPsychological(List<Symptom>symptomList){
+		Map<String,Long> map=symptomList.stream()
+				.filter(s->s.getType()==SymptomType.Psychological)
+				.collect(Collectors.groupingBy(Symptom::getName,Collectors.counting()));
+
+		System.out.println("--- Psychological Symptom ---");
+		map.forEach((s, aLong) -> System.out.println(s+" : "+aLong));
+		System.out.println("-----------------------\n");
+	}
+
+	public static void sortByGlobal(List<Symptom>symptomList){
+		Map<String,Long> map=symptomList.stream()
+				.filter(s->s.getType()==SymptomType.Global)
+				.collect(Collectors.groupingBy(Symptom::getName,Collectors.counting()));
+
+		System.out.println("--- Global Symptom ---");
+		map.forEach((s, aLong) -> System.out.println(s+" : "+aLong));
+		System.out.println("-----------------------\n");
 	}
 }
